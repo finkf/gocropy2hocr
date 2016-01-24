48566aab7788302ffde7260772b9256d6d469e35
flo <finkf@cis.lmu.de>
2016-01-24 21:26:06 +0100

    rename Hocr to HOCR

--- main.go
+++ main.go
@@ -9,18 +9,11 @@ import (
 )
 
 func main() {
-	// llocs, err := gocropy.ReadLlocs(os.Args[1])
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// for i := range llocs {
-	// 	fmt.Printf("%s\n", llocs[i])
-	// }
 	if len(os.Args) != 3 {
 		log.Fatal("Usage: %s <hocr> <dir> ", os.Args[0])
 	}
-	hocr := gocropy.MustReadHocr(os.Args[1])
-	err := hocr.ConvertToHocr(os.Args[2])
+	hocr := gocropy.MustReadHOCR(os.Args[1])
+	err := hocr.ConvertToHOCR(os.Args[2])
 	if err == nil {
 		enc := xml.NewEncoder(os.Stdout)
 		enc.Indent(" ", " ")
